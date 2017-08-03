aab88a7d3f923135c0ccee80d5548a21bd229033
Roman Serikov <roman.serikov@yandex.ru>
2017-08-04 00:21:40 +0300

    Add: Question has many Answers association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803205058) do
+ActiveRecord::Schema.define(version: 20170803210547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170803205058) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
+  it { should belong_to :question }
+  it { should have_db_column(:question_id) }
 end

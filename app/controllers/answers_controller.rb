9ca8296559eec0541b8a0fb5ef39af4999f88c8a
Roman Serikov <roman.serikov@yandex.ru>
2017-08-07 02:57:56 +0300

    Add: action create in Answers Controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,18 @@
 class AnswersController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body, :question_id)
+  end
 end

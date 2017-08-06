7716cd4230af06652f26a7bb82a670048253aeee
Roman Serikov <roman.serikov@yandex.ru>
2017-08-06 22:52:27 +0300

    Refactor: spec for index action in Questions Controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,18 +2,15 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+
+    before { get :index }
+
     it 'populates an array of all questions' do
-      question1 = FactoryGirl.create(:question)
-      question2 = FactoryGirl.create(:question)
-
-      get :index
-
-      expect(assigns(:questions)).to match_array([question1, question2])
+      expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'renders index view' do
-      get :index
-
       expect(response).to render_template :index
     end
   end

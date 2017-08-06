f3bde9534da83a6e1f076dd0278f24b45a6e2fde
Roman Serikov <roman.serikov@yandex.ru>
2017-08-06 23:44:53 +0300

    Refactor: spec for show action in Questions Controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -18,13 +18,13 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #show' do
     let(:question) { create(:question) }
 
+    before { get :show, params: { id: question } }
+
     it 'assigns the requested question to @question' do
-      get :show, params: { id: question }
       expect(assigns(:question)).to eq question
     end
 
     it 'renders show view' do
-      get :show, params: { id: question }
       expect(response).to render_template :show
     end
   end

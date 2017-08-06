a3c611887996dc83181d54bb5f2e20f7a9e6166b
Roman Serikov <roman.serikov@yandex.ru>
2017-08-07 02:32:03 +0300

    Add: spec for create action in Answers Controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'POST #create' do
+    let(:question) { create(:question) }
 
+    context 'with valid attributes' do
+      it 'saves new answer in the database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }
+          .to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to question show view' do 
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+  end
 end

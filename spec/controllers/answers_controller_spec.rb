714e9b100ec98bff1bdcfb833878506593c764e5
Roman Serikov <roman.serikov@yandex.ru>
2017-08-07 03:33:53 +0300

    Add: spec for action new in Answers Controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  describe 'POST #create' do
-    let(:question) { create(:question) }
+  let(:question) { create(:question) }
 
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'assings new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves new answer in the database' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }

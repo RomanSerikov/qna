bc73f184891d976c3936a569a6572e9d9e6e225e
Roman Serikov <roman.serikov@yandex.ru>
2017-08-07 03:27:12 +0300

    Add: spec for create action with invalid attributes in Answers Controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe AnswersController, type: :controller do
         expect(response).to redirect_to question_path(assigns(:question))
       end
     end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) } }
+          .to_not change(Answer, :count)
+      end
+
+      it 're-renders question show view' do
+        post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) }
+        expect(response).to render_template :new
+      end
+    end
   end
 end

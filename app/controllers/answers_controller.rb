4bb9bbf5284e166198cc6e8b17d1f62e34440000
Roman Serikov <roman.serikov@yandex.ru>
2017-08-07 03:37:14 +0300

    Add: new action for AnswersController

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,8 @@
 class AnswersController < ApplicationController
+  def new
+    @answer = Answer.new
+  end
+
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)

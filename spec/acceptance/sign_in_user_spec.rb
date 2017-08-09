c21271774d29c7f7916c734ffca9b2e9bb43a798
Roman Serikov <roman.serikov@yandex.ru>
2017-08-10 02:38:13 +0300

    Add: acceptance spec user sign in

---
+++ spec/acceptance/sign_in_user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User sign in', %q{
+  In order to be able to ask question
+  As a user
+  I want to be able to sign in
+} do
+
+  scenario 'User try to sign in' do
+    User.create!(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+    expect(current_page).to eq root_path
+  end
+end

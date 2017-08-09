bd77bb33b5629fd7932b95e34d62d32838e3b92b
Roman Serikov <roman.serikov@yandex.ru>
2017-08-09 20:05:48 +0300

    Add: create question views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question succefully created.'
     else
       render :new
     end

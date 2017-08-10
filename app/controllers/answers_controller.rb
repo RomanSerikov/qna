d53bd5efe1f95fcbda690220cb9b48f031654cc4
Roman Serikov <roman.serikov@yandex.ru>
2017-08-10 23:14:47 +0300

    Add: two scenarios to create answer spec

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -10,7 +10,7 @@ class AnswersController < ApplicationController
     if @answer.save
       redirect_to @question, notice: 'Your answer succefully created.'
     else
-      render :new
+      render 'questions/show'
     end
   end
 
--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -6,7 +6,14 @@ feature 'Create answer', %q{
   I want to be able to answer the question
 } do
 
-  scenario 'User creates answer' do
+  scenario 'Authenticated user creates answer with valid parameters' do
+    User.create!(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
     visit questions_path
     click_on 'Ask question'
     fill_in 'Title', with: 'Test question'
@@ -18,5 +25,32 @@ feature 'Create answer', %q{
 
     expect(page).to have_content 'Your answer succefully created.'
     expect(page).to have_content 'Answer on test question'
+  end
+
+  scenario 'Authenticated user tries to create answer with invalid parameters' do
+    User.create!(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'test test test'
+    click_on 'Create'
+
+    fill_in 'Answer', with: nil
+    click_on 'Send answer'
+
+    expect(page).to have_content 'Body can\'t be blank'
+  end
+
+  scenario 'Non-authenticated user tries to create answer' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end

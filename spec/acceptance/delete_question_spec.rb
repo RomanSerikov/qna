141f634a803ff2690625013f57b8dd36a9c155d8
Roman Serikov <roman.serikov@yandex.ru>
2017-08-11 00:11:49 +0300

    Add: acceptance spec delete question

---
+++ spec/acceptance/delete_question_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature '', %q{
+  In order to fix my mistake
+  As a user
+  I want to be able to delete my question
+} do
+  
+  scenario 'Authenticated user deletes own question' do
+    User.create!(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'test test test'
+    click_on 'Create'
+
+    click_on 'Delete my question'
+    expect(current_path).to eq questions_path
+    expect(page).to have_content 'Your question succefully removed.'
+    expect(page).to_not have_content 'Test question'
+  end
+end

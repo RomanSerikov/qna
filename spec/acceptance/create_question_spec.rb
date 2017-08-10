3205f966e71c2a754ec4ee792513ffb96767d81e
Roman Serikov <roman.serikov@yandex.ru>
2017-08-10 22:47:43 +0300

    Add: two scenarios to create question spec

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -24,4 +24,29 @@ feature 'Create question', %q{
     expect(page).to have_content 'Test question'
     expect(page).to have_content 'test test test'
   end
+
+  scenario 'Authenticated user tries to create question with invalid parameters' do
+    User.create!(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: nil
+    fill_in 'Body', with: nil
+    click_on 'Create'
+
+    expect(page).to have_content 'Body can\'t be blank'
+    expect(page).to have_content 'Title can\'t be blank'
+  end
+
+  scenario 'Non-authenticated user tries to create question' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
 end

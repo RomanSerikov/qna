d143ba6c76b37948fcf83575a2c0dfbb7fd39a18
Roman Serikov <roman.serikov@yandex.ru>
2017-08-12 00:00:58 +0300

    Add: belongs_to tests to question and answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should have_db_column :question_id }
+  it { should belong_to(:user) }
+  it { should have_db_column(:user_id) }
 
   it { should validate_presence_of :body }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { should belong_to(:user) }
+  it { should have_db_column(:user_id) }
+
   it { should have_many(:answers).dependent(:destroy) }
 
   it { should validate_presence_of :title }

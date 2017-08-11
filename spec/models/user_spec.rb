6dfb53a7ecfb37b5f073311443f0f1819a3fe4af
Roman Serikov <roman.serikov@yandex.ru>
2017-08-12 00:24:28 +0300

    Refactor: user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,19 @@ RSpec.describe User, type: :model do
   it { should have_many(:questions).dependent(:destroy) }
   it { should have_many(:answers).dependent(:destroy) }
 
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+
   describe '#owner_of?' do
     let(:owner)     { create(:user) }
     let(:not_owner) { create(:user) }
     let(:question)  { create(:question, user: owner) }
 
-    it 'correctly identify owner' do
+    it 'returns true when user is owner' do
       expect(owner.owner_of?(question)).to be true
+    end
+
+    it 'return false when user is not owner' do
       expect(not_owner.owner_of?(question)).to be false
     end
   end

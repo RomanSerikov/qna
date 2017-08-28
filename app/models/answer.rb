d0ab88f27e0a35e7b640a4a8169d8c07adee7d9b
Roman Serikov <roman.serikov@yandex.ru>
2017-08-28 18:56:15 +0300

    Add: polymorphic association for answers and attachments.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,16 +1,20 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+
+  has_many :attachments, dependent: :destroy, as: :attachable
   
   validates :body, presence: :true
   validates :best, uniqueness: { scope: :question_id }, if: :best
+
+  accepts_nested_attributes_for :attachments
 
   default_scope { order(best: :desc) }
 
   def mark_best
     transaction do
       question.answers.update_all(best: false)
-      self.update(best: true)
+      update(best: true)
     end
   end
 end

5a912cf4f58800bf77fbbd54ef54ee02a495c417
Roman Serikov <roman.serikov@yandex.ru>
2017-09-04 21:47:41 +0300

    Add: validation for user can vote only once

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,14 @@
 class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true, optional: true
   belongs_to :user
+
+  validate :user_can_vote_only_once
+
+  private
+
+  def user_can_vote_only_once
+    if Vote.where(user_id: user).where(votable_id: votable).exists?
+      errors.add(:votes_count, "can't be more than one")
+    end
+  end
 end

80618b9157ba5060168d4b448d370a443af1fa3b
Roman Serikov <roman.serikov@yandex.ru>
2017-09-04 20:48:44 +0300

    Add: can_vote? helpers for question and answer

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -2,4 +2,8 @@ module AnswersHelper
   def can_mark_best?(question, answer)
     user_signed_in? && current_user.owner_of?(question) && !answer.best
   end
+
+  def can_vote?(answer)
+    user_signed_in? && !current_user.owner_of?(answer)
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,5 @@
 module QuestionsHelper
+  def can_vote?(question)
+    user_signed_in? && !current_user.owner_of?(question)
+  end
 end

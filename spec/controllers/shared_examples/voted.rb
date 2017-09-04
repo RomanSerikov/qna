57bae91286407daaac1e2263028322312f5d620d
Roman Serikov <roman.serikov@yandex.ru>
2017-09-04 20:30:25 +0300

    Fix: questions and answers factories

--- spec/controllers/shared_examples/voted.rb
+++ spec/controllers/shared_examples/voted.rb
@@ -3,22 +3,27 @@ require 'rails_helper'
 shared_examples 'voted' do
   resource_name = described_class.controller_name.singularize.to_sym
 
-  let(:user)     { create(:user) }
-  let(:resource) { create(resource_name, user: user) }
+  let(:resource)        { create(resource_name) }
+  let(:question)        { create(:question) }
+  let(:resource_params) { { id: resource } }
+
+  before do
+    resource_params[:question_id] = question if resource_name == :answer
+  end
 
   sign_in_user
 
   describe 'POST #voteup' do
     it 'increase votes by 1' do
-      post :voteup, params: { id: resource }, format: :js
-      expect(resource.votes.sum(:value)).to eq(1)
+      post :voteup, params: resource_params, format: :js
+      expect(resource.rating).to eq(1)
     end
   end
 
   describe 'POST #votedown' do
     it 'decrease votes by 1' do
-      post :votedown, params: { id: resource }, format: :js
-      expect(resource.votes.sum(:value)).to eq(-1)
+      post :votedown, params: resource_params, format: :js
+      expect(resource.rating).to eq(-1)
     end
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :question do
     title "MyQuestionTitle"
     body "MyQuestionText"
+    user
   end
 
   factory :invalid_question, class: "Question" do
     title nil
     body nil
+    user
   end
 end

94adfccb97e835f6c71cb8df59e679d51ae548d4
Roman Serikov <roman.serikov@yandex.ru>
2017-09-07 22:44:35 +0300

    Refactor: move can_vote? helper to ApplicationHelper

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -2,8 +2,4 @@ module AnswersHelper
   def can_mark_best?(question, answer)
     user_signed_in? && current_user.owner_of?(question) && !answer.best
   end
-
-  def can_vote?(answer)
-    user_signed_in? && !current_user.owner_of?(answer)
-  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,2 @@
 module QuestionsHelper
-  def can_vote?(question)
-    user_signed_in? && !current_user.owner_of?(question)
-  end
 end

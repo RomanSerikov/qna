ca2b03576f9fa57ad746f8e436e8af85bd31ac63
Roman Serikov <roman.serikov@yandex.ru>
2017-10-09 20:58:27 +0300

    Add: namespace routes for api v1

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,13 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resource :profiles do
+        get :me, on: :collection
+      end
+    end
+  end
+
   mount ActionCable.server => '/cable'
 end

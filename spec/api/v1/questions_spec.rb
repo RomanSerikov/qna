39011bc2d80d6384d0cb94c3690d258881e67f08
Roman Serikov <roman.serikov@yandex.ru>
2017-10-17 23:52:37 +0300

    Add: spec for Question show api action

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -55,4 +55,62 @@ describe 'Questions API' do
       end
     end
   end
+
+  describe 'GET /show' do
+    let(:question) { create(:question) }
+
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access token' do
+        get "/api/v1/questions/#{question.id}", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access token is invalid' do
+        get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let!(:comment)     { create(:question_comment, commentable: question) }
+      let!(:attachment)  { create(:attachment, attachable: question) }
+
+      before do 
+        get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: access_token.token }
+      end
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("question/#{attr}")
+        end
+      end
+
+      context 'attachments' do
+        it 'included in question object' do
+          expect(response.body).to have_json_size(1).at_path("question/attachments")
+        end
+
+        it 'contains attachment url' do
+          expect(response.body).to be_json_eql(attachment.file.url.to_json).at_path("question/attachments/0/url")
+        end
+      end
+
+      context 'comments' do
+        it 'included in question object' do
+          expect(response.body).to have_json_size(1).at_path("question/comments")
+        end
+
+        %w(id body commentable_type commentable_id user_id created_at updated_at).each do |attr|
+          it "contains #{attr}" do
+            expect(response.body).to be_json_eql(comment.send(attr.to_sym).to_json).at_path("question/comments/0/#{attr}")
+          end
+        end
+      end
+    end
+  end
 end
--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :attachment do
     file { File.new("#{Rails.root}/spec/spec_helper.rb") }
+    attachable nil
   end
 end

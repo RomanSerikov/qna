ed5db641f2fb24228fb2972f1092431cdfbe5870
Roman Serikov <roman.serikov@yandex.ru>
2017-10-19 21:20:50 +0300

    Refactor: separate attachable from question and answer model specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,18 +3,15 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it_behaves_like 'votable'
   it_behaves_like 'commentable'
+  it_behaves_like 'attachable'
 
   it { should belong_to :question }
   it { should have_db_column :question_id }
   it { should belong_to(:user) }
   it { should have_db_column(:user_id) }
 
-  it { should have_many(:attachments).dependent(:destroy) }
-
   it { should validate_presence_of :body }
   it { should validate_uniqueness_of(:best).scoped_to(:question_id) }
-
-  it { should accept_nested_attributes_for :attachments }
 
   describe '#mark_best' do
     let(:user)     { create(:user) }
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it_behaves_like 'votable'
   it_behaves_like 'commentable'
+  it_behaves_like 'attachable'
 
   it { should belong_to(:user) }
   it { should have_db_column(:user_id) }
 
   it { should have_many(:answers).dependent(:destroy) }
-  it { should have_many(:attachments).dependent(:destroy) }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-
-  it { should accept_nested_attributes_for :attachments }
 end

6b90249d64515bf212ede0a08b7622866231fea8
Roman Serikov <roman.serikov@yandex.ru>
2017-10-29 02:37:20 +0300

    Add: spec for daily digest job

--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -1,5 +1,8 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe DailyDigestJob, type: :job do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe DailyDigestJob, type: :job do
+  it 'sends daily digets' do
+    expect(User).to receive(:send_daily_digest)
+    DailyDigestJob.perform_now
+  end
+end

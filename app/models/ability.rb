9b4faaa529f20dd49570de718a1556014a3cf4f8
Roman Serikov <roman.serikov@yandex.ru>
2017-10-29 02:36:32 +0300

    Fix: subscription ability and views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,6 +20,7 @@ class Ability
   def set_aliases
     alias_action :update, :destroy, :to => :modify
     alias_action :voteup, :votedown, :to => :vote
+    alias_action :create, :destroy, :to => :use
   end
 
   def guest_abilities
@@ -32,7 +33,7 @@ class Ability
 
   def user_abilities
     guest_abilities
-    can :create, [Question, Answer, Comment, Subscription]
+    can :create, [Question, Answer, Comment]
     can :modify, [Question, Answer], user: user
     can :vote, [Question, Answer]
     cannot :vote, [Question, Answer], user: user
@@ -41,6 +42,6 @@ class Ability
     end
     can :me, User
     can :index, User
-    can :destroy, Subscription
+    can :use, Subscription
   end
 end

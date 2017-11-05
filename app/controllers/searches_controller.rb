f8a2a060a46504f9dd645fa67cc59718f7dc381b
Roman Serikov <roman.serikov@yandex.ru>
2017-11-05 16:25:20 +0300

    Add: check for search type

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,6 +8,7 @@ class SearchesController < ApplicationController
   end
 
   def create
+    redirect_to new_search_path if check_search_type
     @search = model_klass.search search_params[:text]
     render :show
   end
@@ -30,5 +31,9 @@ class SearchesController < ApplicationController
 
   def model_klass
     search_params[:type].to_s.camelize.constantize
+  end
+
+  def check_search_type
+    @search_types.value?(search_params[:type])
   end
 end

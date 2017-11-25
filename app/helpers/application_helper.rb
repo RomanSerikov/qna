4b69e628354e3d8f95ee3a7f4a808a52d6da52b2
Roman Serikov <roman.serikov@yandex.ru>
2017-11-26 00:30:52 +0300

    Add: some styles via bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def flash_helper
     content_tag :div, class: "flash" do
       flash.map do |key, value| 
-        content_tag :div, value, class: key 
+        content_tag :div, value, class: "alert alert-#{key}" 
       end.join.html_safe
     end
   end

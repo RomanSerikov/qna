6bd3f0b1afe6bfedc5cba055df63b4165b7134ee
Roman Serikov <roman.serikov@yandex.ru>
2018-01-29 21:14:22 +0300

    Fix: spec for sending digest only if questions exists

--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe DailyDigestJob, type: :job do
+  let!(:questions) { create_list(:question, 3) }
+  
   it 'sends daily digets' do
     expect(User).to receive(:send_daily_digest)
     DailyDigestJob.perform_now
